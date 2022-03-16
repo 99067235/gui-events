003ce83cec0d7fc76d47b4a9f5b9300c759274d0
unknown <99067235@mydavinci.nl>
2022-03-16 10:32:33 +0100

    fps trainer

--- FPS trainer v1.py
+++ FPS trainer v1.py
@@ -4,26 +4,24 @@ import tkinter
 import random
 import time
 
-actions  = ["press w", "press a", "press s", "press d"]
+actions  = ["press w", "press a", "press s", "press d", "double click"]
 root = tk.Tk()
 root.configure(bg="black")
 root.geometry("700x500")
 
-timerLabel = Label(root, text=20, height= 2, width= 10)
-timerLabel.pack()
-timerLabel.place(x=0, y=0)
+
 scoreLabel = Label(root, text=0, height=2, width=30)
 scoreLabel.pack()
 def key_pressed(event):
-    if event.char == randomText and randomText == "w" or event.char == randomText and randomText == "a" or event.char == randomText and randomText == "s" or event.char == randomText and randomText == "d":
+    if event.char == randomText:
         scoreLabel["text"] += 1
         actionLabel.destroy()
         action()
     else:
         key()
 
-
 def action():
+
     global randomText, actionLabel
     start.destroy()
     randomText = random.choice(actions)
@@ -45,12 +43,28 @@ def action():
 
 def key():
     root.bind("<Key>",key_pressed)
-    
+
 
 start = tk.Button(root, text="Click here to start", command=action)
 start.pack()
 
+now = 3
+class Clock():
+    def __init__(self):
+        self.window = root
+        self.label = tk.Label(root, text=20, height= 2, width= 10)
+        self.label.pack()
+        self.label.place(x=0, y=0)
+        self.update_clock()
+        self.window.mainloop()
 
+    def update_clock(self):
+        global now
+        self.label.configure(text=now)
+        now = now - 1
+        self.window.after(1000, self.update_clock)
+        if now == 0:
+            self.window.destroy()
 
-
+app=Clock()
 root.mainloop()
